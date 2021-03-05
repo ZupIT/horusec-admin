d5b08c8ab3f1d8e2b0efbb4d0f1457ef6ab488fc
Tiago Angelo <tiago.angelo@zup.com.br>
2021-03-05 10:56:47 -0300

    Fixing spec components

--- internal/business/adapter/manager.go
+++ internal/business/adapter/manager.go
@@ -9,16 +9,13 @@ import (
 )
 
 func newManager(cr *api.HorusecManager) *core.Manager {
+	mng := new(core.Manager)
 	components := cr.Spec.Components
-	api := (&url.URL{Scheme: components.API.Ingress.Scheme, Host: components.API.Ingress.Host}).String()
-	analytic := (&url.URL{Scheme: components.Analytic.Ingress.Scheme, Host: components.Analytic.Ingress.Host}).String()
-	account := (&url.URL{Scheme: components.Account.Ingress.Scheme, Host: components.Account.Ingress.Host}).String()
-	auth := (&url.URL{Scheme: components.Auth.Ingress.Scheme, Host: components.Auth.Ingress.Host}).String()
-	return &core.Manager{
-		APIEndpoint:      api,
-		AnalyticEndpoint: analytic,
-		AccountEndpoint:  account,
-		AuthEndpoint:     auth,
-		ManagerPath:      "", // TODO: add this field to the Operator
+	if components != nil {
+		mng.APIEndpoint = (&url.URL{Scheme: components.API.Ingress.Scheme, Host: components.API.Ingress.Host}).String()
+		mng.AnalyticEndpoint = (&url.URL{Scheme: components.Analytic.Ingress.Scheme, Host: components.Analytic.Ingress.Host}).String()
+		mng.AccountEndpoint = (&url.URL{Scheme: components.Account.Ingress.Scheme, Host: components.Account.Ingress.Host}).String()
+		mng.AuthEndpoint = (&url.URL{Scheme: components.Auth.Ingress.Scheme, Host: components.Auth.Ingress.Host}).String()
 	}
+	return mng
 }
