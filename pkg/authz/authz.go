46c2f388443d54ded9f6a821cd581d96ede2bc5d
Nathan Tavares Nascimento <nathan.nascimento@zup.com.br>
2021-03-10 10:38:45 -0300

    Adding authz token interval cycle

--- pkg/authz/authz.go
+++ pkg/authz/authz.go
@@ -7,29 +7,58 @@ import (
 )
 
 const TokenLength = 20
+const RefreshTokenInterval = 2 * time.Hour
 
 type Authz struct {
-	token     string
-	createdAt time.Time
+	token                   string
+	createdAt               time.Time
+	quitRefreshTokenCycleCn chan struct{}
 }
 
 func NewAuthz() *Authz {
 	authz := &Authz{}
-	_ = authz.generateToken()
+
+	authz.generateToken()
+	authz.startRefreshTokenCycle()
 
 	return authz
 }
 
-func (a *Authz) generateToken() error {
+func (a *Authz) startRefreshTokenCycle() {
+	ticker := time.NewTicker(RefreshTokenInterval)
+	a.quitRefreshTokenCycleCn = make(chan struct{})
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				a.generateToken()
+			case <-a.quitRefreshTokenCycleCn:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+}
+
+func (a *Authz) generateToken() {
+	token, _ := a.getRandTokenString()
+
+	a.token = token
+	a.createdAt = time.Now()
+}
+
+func (a *Authz) getRandTokenString() (string, error) {
 	b := make([]byte, TokenLength)
 	if _, err := rand.Read(b); err != nil {
-		return err
+		return "", err
 	}
 
-	a.token = hex.EncodeToString(b)
-	a.createdAt = time.Now()
+	return hex.EncodeToString(b), nil
+}
 
-	return nil
+func (a *Authz) Stop() {
+	close(a.quitRefreshTokenCycleCn)
 }
 
 func (a *Authz) GetToken() string {
